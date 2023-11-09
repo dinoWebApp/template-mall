83a42ce6756cae373042650c9d0f67b3c711f8c6
nomadWebDeveloper <kgjsmart1559@gmail.com>
2023-11-09 22:52:44 +0900

    finish homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,152 @@
 @extends('layouts.header')
+@section('title', 'Home')
+@section('content')
+<!-- 광고 -->
+<div class="container mt-4 d-flex justify-content-center">
+  <div class="ads">
+    <div class="ads-slide">
+      <img src="{{asset('images/ads1.PNG')}}" class="img-fluid" alt="ads">
+    </div>
+    <div class="ads-slide">
+      <img src="{{asset('images/ads1.PNG')}}" class="img-fluid" alt="ads">
+    </div>
+    <div class="ads-slide">
+      <img src="{{asset('images/ads1.PNG')}}" class="img-fluid" alt="ads">
+    </div>
+    <div class="ads-slide">
+      <img src="{{asset('images/ads1.PNG')}}" class="img-fluid" alt="ads">
+    </div>
+  </div>
+</div>
+
+<!-- 인기상품 -->
+<div class="container mt-4">
+  <p class="fs-5 mb-4"><i class="bi bi-heart-fill text-danger me-2"></i>인기상품 TOP5</p>
+  <!-- 상품 리스트 -->
+  <div class="d-flex justify-content-around">
+    <div class="products-top5 col-2">
+      <div>
+        <img src="{{asset('images/top-products.jpeg')}}" class="img-fluid" alt="">
+      </div>
+      <p class="category">스티커</p>
+      <p class="product-title">다양한 굿노트 꽃 스티커</p>
+      <p class="price">3,000원</p>
+    </div>
+    <div class="products-top5 col-2">
+      <img src="{{asset('images/top-products.jpeg')}}" class="img-fluid" alt="">
+      <p class="category">다이어리/템플릿</p>
+      <p class="product-title">2023 심플 그린 굿노트 다이어리 (스티커 선택)</p>
+      <p class="price">2,000원</p>
+    </div>
+    <div class="products-top5 col-2">
+      <img src="{{asset('images/top-products.jpeg')}}" class="img-fluid" alt="">
+      <p class="category">노트</p>
+      <p class="product-title">그리드 격자 노트 (색상 선택)</p>
+      <p class="price">1,000원</p>
+    </div>
+    <div class="products-top5 col-2">
+      <img src="{{asset('images/top-products.jpeg')}}" class="img-fluid" alt="">
+    </div>
+    <div class="products-top5 col-2">
+      <img src="{{asset('images/top-products.jpeg')}}" class="img-fluid" alt="">
+    </div>
+  </div>
+</div>
+
+<!-- 전체상품 -->
+<div class="container mt-5">
+  <p class="total-product fs-5 mb-4"><i class="bi bi-star-fill me-2 text-warning"></i></i>전체 상품 <a href="#" class="fs-6 text-secondary">더보기</a></p>
+  <!-- 전체 상품 리스트 -->
+  <div class="d-flex justify-content-around flex-wrap">
+    @for($i = 0; $i < 10; $i++)
+    <div class="products-top5 mx-1 col-2">
+      <div>
+        <img src="{{asset('images/top-products.jpeg')}}" class="img-fluid" alt="">
+      </div>
+      <p class="category">스티커</p>
+      <p class="product-title">다양한 굿노트 꽃 스티커</p>
+      <p class="price">3,000원</p>
+    </div>
+    @endfor
+  </div>
+</div>
+
+<script>
+  $('.ads').slick({
+    dots : true,
+    infinite : true,
+    speed : 300,
+    slidesToShow : 1,
+    slideToScroll : 1,
+    arrows : true,
+    autoplay: true,
+    autoplaySpeed : 3000,
+    draggable : true,
+    centerMode: true,
+    centerPadding: '140px',
+    prevArrow : "<button type='button' class='slick-prev' style='color : red;'>Previous</button>"
+  });
+</script>
+<style>
+  .ads {
+    width: 95%;
+  }
+
+  .ads-slide{
+    margin-right: 30px;
+    margin-left: 30px;
+  }
+
+  .slick-prev:before,
+  .slick-next:before {
+    color: red;
+  }
+
+  .products-top5 {
+    height: 350px;
+    position: relative;
+  }
+
+  .products-top5 img {
+    border-radius: 10px;
+  }
+
+  .category {
+    margin-top: 10px;
+    font-size: small;
+    color: gray;
+  }
+  .product-title {
+    margin-top: -15px;
+  }
+  .price {
+    position: absolute;
+    bottom: 1px;
+  }
+
+  .total-product {
+    position: relative;
+  }
+
+  .total-product a {
+    position: absolute;
+    right: 25px;
+  }
+
+  @media screen and (max-width: 1400px) {
+    .products-top5 {
+      height: 300px;
+    }
+  }
+
+  @media screen and (max-width: 1200px) {
+    .products-top5 {
+      height: 270px;
+    }
+  }
+</style>
+
+@endsection
+
+
+
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-  <title>Header</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  
+  <title>@yield('title')</title>
 </head>
 <body>
   <!-- 헤더 -->
-  <div class="container mt-2">
+  <div class="container mt-2 sticky-top" style="background-color: white;">
     <!-- 상단 로고 검색 로그인 가입 -->
     <div class="d-flex" style="height: 60px;">
       <!-- 로고 -->
@@ -23,7 +29,6 @@
           <input type="text" class="form-control w-75" placeholder="굿노트 다이어리를 검색해보세요!" style="background-color: #fff9f9;">
           <a href="#" class="input-group-text"><i class="bi bi-search"></i></a>
         </div>
-        
       </div>
 
       <!-- 장바구니 마이페이지 -->
@@ -46,38 +51,41 @@
         </div>
       </div>
       <!-- 로그인 회원가입 -->
-      <div class="col-3 d-flex align-items-center fs-6  ">
+      <div class="col-2 d-flex align-items-center fs-6  ">
         <a href="" class="text-decoration-none text-reset">로그인</a>
         <a href="" class="text-decoration-none text-reset ms-4">회원가입</a>
       </div>
     </div>
     <hr class="mb-1">
     <!-- navbar -->
-    <div class="d-flex">
+    <div class="d-flex" id="navbar">
       <div class="d-flex justify-content-center align-items-center ms-2">
-        <a href="" class="d-flex text-decoration-none text-reset align-items-center" >
+        <button class="d-flex align-items-center" >
           <i class="bi bi-list fs-4 me-1"></i>
           전체상품
-        </a>
+        </button>
       </div>
       <div class="d-flex justify-content-center align-items-center ms-5">
-        <a href="" class="d-flex text-decoration-none text-reset align-items-center">
+        <button class="d-flex align-items-center">
           노트
-        </a>
+        </button>
       </div>
       <div class="d-flex justify-content-center align-items-center ms-5">
-        <a href="" class="d-flex text-decoration-none text-reset align-items-center">
+        <button href="" class="d-flex align-items-center">
           다이어리/플래너
-        </a>
+        </button>
       </div>
       <div class="d-flex justify-content-center align-items-center ms-5">
-        <a href="" class="d-flex text-decoration-none text-reset align-items-center">
+        <button class="d-flex text-decoration-none text-reset align-items-center">
           스티커
-        </a>
+        </button>
       </div>
     </div>
     <hr class="my-1">
   </div>
+
+  @yield('content')
+  
 </body>
 <style>
   body {
@@ -85,6 +93,11 @@
   }
   div {
     box-sizing: border-box;
+  }
+
+  #navbar div button {
+    border: none;
+    background-color: rgba(0, 0, 0, 0);
   }
 </style>
 </html>
